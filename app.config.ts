7f28694d662a01f9782008c1f53b0b07691d1755
wnewsadmin <162071372+wnewsadmin@users.noreply.github.com>
2024-03-04 14:47:29 +1100

    added skeleton

--- app.config.ts
+++ app.config.ts
@@ -1,7 +1,7 @@
 export default defineAppConfig({
   alpine: {
-    title: 'Alpine',
-    description: 'The minimalist blog theme',
+    title: 'Waranga News',
+    description: 'Serving the community for 40 years',
     image: {
       src: '/wnews-masthead.png',
       alt: 'An image showcasing my project.',
@@ -11,23 +11,28 @@ export default defineAppConfig({
     header: {
       position: 'right', // possible value are : | 'left' | 'center' | 'right'
       logo: {
-        path: '/logo.svg', // path of the logo
+        path: '/wnews_p5.png', // path of the logo
         pathDark: '/logo-dark.svg', // path of the logo in dark mode, leave this empty if you want to use the same logo
-        alt: 'alpine' // alt of the logo
+        alt: 'Waranga News logo' // alt of the logo
       }
     },
     footer: {
       credits: {
-        enabled: true, // possible value are : true | false
-        repository: 'https://www.github.com/nuxt-themes/alpine' // our github repository
+        // possible value are : true | false
+        enabled: true,
+
+        // our github repository
+        repository: 'https://www.github.com/nuxt-themes/alpine',
+
+        text: 'Copyright 2024 - Waranga News Inc.'
       },
       navigation: true, // possible value are : true | false
       alignment: 'center', // possible value are : 'none' | 'left' | 'center' | 'right'
       message: 'Follow me on' // string that will be displayed in the footer (leave empty or delete to disable)
     },
     socials: {
-      twitter: 'nuxtlabs',
-      instagram: 'atinuxt',
+      twitter: 'WarangaNews',
+      instagram: '',
       linkedin: {
         icon: 'uil:linkedin',
         label: 'LinkedIn',
